2012a3449cc6b367fadebc973698bed2955e60d6
Ramesh Yoha <ramesh.yohalingam@gmail.com>
2024-06-06 16:38:19 -0400

    Added linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,19 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'prettier'
   ],
+  plugins: [`prettier`],
   parserOptions: {
+    sourceType: `module`,
     ecmaVersion: 'latest'
+  },
+  env: {
+    node: true,
+    es2020: true
   }
 }
